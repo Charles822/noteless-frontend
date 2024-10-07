3ca9647120f298da51abbe8642c02183590e11af
Charles Fauchet <charles.fauchet@gmail.com>
2024-10-07 10:01:13 +0800

    Reset baseURLto dev url

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseURL = process.env.REACT_APP_API_BASE_URL_PROD;
+export const baseURL = process.env.REACT_APP_API_BASE_URL;
 
 const createAxiosInstance = () => {
   const token = localStorage.getItem('authTokens');
