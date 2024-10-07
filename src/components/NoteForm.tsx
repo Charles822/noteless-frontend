1816e5da6e91a401c4e19438cfdc3183171f2b55
Charles Fauchet <charles.fauchet@gmail.com>
2024-10-08 07:25:37 +0800

    Try modifying logs on useInterval

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -65,11 +65,12 @@ useInterval(async () => {
   if (taskIds.length > 0) {
     for (const taskId of taskIds) {
       try {
+        console.log('Fetching task status...');
         const response = await fetch(`${baseURL}/notes/notes/check_task_status/${taskId}/`, {
           method: 'GET',
         });
         const data = await response.json();
-        console.log('Task status:', data);
+        console.log('Task status: it should be processing');
         if (data.status === 'SUCCESS') {
           toast({ variant: "success", description: "Your note is ready!" });
           setTaskIds(prevTaskIds => prevTaskIds.filter(id => id !== taskId));
