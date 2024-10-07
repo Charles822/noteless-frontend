e95e2e8cef0534094305aaf755016f20f2545ac1
Charles Fauchet <charles.fauchet@gmail.com>
2024-10-07 09:02:08 +0800

    Added baseURL for production backend server

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-export const baseURL = process.env.REACT_APP_API_BASE_URL;
+export const baseURL =
+  process.env.NODE_ENV === 'production'
+    ? process.env.REACT_APP_API_BASE_URL_PROD
+    : process.env.REACT_APP_API_BASE_URL_DEV;
 
 const createAxiosInstance = () => {
   const token = localStorage.getItem('authTokens');
