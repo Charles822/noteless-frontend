31cb1606e970423a953984dd2432ce8bd6d4a1c2
Charles Fauchet <charles.fauchet@gmail.com>
2024-10-10 11:24:51 +0900

    Set up new useInterval with types

--- src/hooks/useInterval.ts
+++ src/hooks/useInterval.ts
@@ -1,32 +1,8 @@
 import { useEffect, useRef } from 'react';
  
-// this is not a built-in hook from React
-// export function useInterval(callback: () => void, delay: number | null) {
-//   const savedCallback = useRef<() => void>();
- 
-//   // Remember the latest callback.
-//   useEffect(() => {
-//     savedCallback.current = callback;
-//   }, [callback]);
- 
-//   // Set up the interval.
-//   useEffect(() => {
-//     function tick() {
-//       if (savedCallback.current) {
-//         savedCallback.current();
-//       }
-//     }
-//     if (delay !== null) {
-//       let id = setInterval(tick, delay);
-//       return () => clearInterval(id);
-//     }
-//   }, [delay]);
-// }
-
-
-// this is not a built-in hook from React
-export function useInterval(callback, delay) {
-  const savedCallback = useRef();
+this is not a built-in hook from React
+export function useInterval(callback: () => void, delay: number | null) {
+  const savedCallback = useRef<() => void>();
  
   // Remember the latest callback.
   useEffect(() => {
@@ -36,7 +12,9 @@ export function useInterval(callback, delay) {
   // Set up the interval.
   useEffect(() => {
     function tick() {
-      savedCallback.current();
+      if (savedCallback.current) {
+        savedCallback.current();
+      }
     }
     if (delay !== null) {
       let id = setInterval(tick, delay);
@@ -44,3 +22,25 @@ export function useInterval(callback, delay) {
     }
   }, [delay]);
 }
+
+
+// this is not a built-in hook from React
+// export function useInterval(callback, delay) {
+//   const savedCallback = useRef();
+ 
+//   // Remember the latest callback.
+//   useEffect(() => {
+//     savedCallback.current = callback;
+//   }, [callback]);
+ 
+//   // Set up the interval.
+//   useEffect(() => {
+//     function tick() {
+//       savedCallback.current();
+//     }
+//     if (delay !== null) {
+//       let id = setInterval(tick, delay);
+//       return () => clearInterval(id);
+//     }
+//   }, [delay]);
+// }
